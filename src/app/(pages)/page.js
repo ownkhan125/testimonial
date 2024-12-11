eb3310cfafdc514621c7fe8d42dbe4acb3276bda
ownkhan125 <107195320+ownkhan125@users.noreply.github.com>
2024-12-11 17:02:17 +0500

    with middleware.

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -1,16 +1,11 @@
-'use client'
-
-import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const page = () => {
-  const { data: session } = useSession();
 
-  console.log('check the session', session);
   return (
     <>
       <div className='container-1'>
-        <h1>home page {session?.user?.userId}</h1>
+        <h1>home page</h1>
       </div>
     </>
   )
